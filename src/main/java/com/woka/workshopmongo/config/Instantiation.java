f507f8e08ce9aa95f277f92b4d80fd4f6e44eed9
Mauri Junior <mvrjunior26@gmail.com>
2024-04-24 15:51:24 -0300

    Protecao dos dados do autor com DTO

--- src/main/java/com/woka/workshopmongo/config/Instantiation.java
+++ src/main/java/com/woka/workshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.woka.workshopmongo.domain.Post;
 import com.woka.workshopmongo.domain.User;
+import com.woka.workshopmongo.dto.AuthorDTO;
 import com.woka.workshopmongo.repository.PostRepository;
 import com.woka.workshopmongo.repository.UserRepository;
 
@@ -35,8 +36,10 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para sao paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para sao paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
 		postRepository.saveAll(Arrays.asList(post1, post2));
